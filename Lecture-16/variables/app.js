ab29ce382680612a3882704671d955753536d8b2
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-09-18 22:21:39 +0530

    lecture 16 codes

---
+++ Lecture-16/variables/app.js
@@ -0,0 +1,49 @@
+
+// let sam = 200;
+// let sam1 = 100;
+
+// ------- strings -------
+// way-1
+// let firstName = 'samarth';
+// let lastName = 'vohra';
+// let space = ' '
+
+// way-2
+// let firstName = "samarth";
+// let lastName = "vohra";
+// let space = " "
+
+// way-3
+// let firstName = `samarth`;
+// let lastName = `vohra`;
+// let space = ` `
+
+// console.log(firstName)
+// console.log(lastName)
+
+// ------------------------
+// string concatination (concat) -> +
+// console.log(firstName + lastName)
+// console.log(firstName + space + lastName)
+// console.log(firstName + ' ' + lastName)
+// console.log(firstName + " " + lastName)
+// console.log(firstName + ` ` + lastName)
+
+
+// ----------------------------------
+
+// let houseNaam = 'samarth vohra's house' //wrong
+let houseNaam = "samarth vohra's house" //right
+let houseNaam2 = 'samarth vohra\'s house' //right
+
+// let baarish = 'hogi\nNot'
+// let baarish = "hogi\nNot"
+// let baarish2 = 'hogi'+ "\n" +'Not';
+
+
+
+
+
+
+
+
