06c556752bcb8e944c127e33d4cec316e343ee15
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-10-02 21:54:38 +0530

    lecture 22 codes

---
+++ Lecture-22/execution-context/app.js
@@ -0,0 +1,48 @@
+// var username = "samarth";
+
+// var person = {
+//     email : "sam@gmail.com",
+//     password: "samarth"
+// }
+
+
+// console.log(username);
+
+// -----------------------------
+
+
+// var username = "devi prasad";
+
+// function sum(){
+//     var num1 = 10;
+//     var num2 = 20;
+
+//     return num1+num2;
+// }
+
+// sum();
+// console.log(username)
+
+
+// -------------------------------
+
+var naam = "samarth"
+
+function sam(){
+    console.log("inside sam");
+    var a = 10;
+    console.log(a);
+}
+
+sam();
+console.log(naam);
+
+
+
+
+
+
+
+
+
+
---
+++ Lecture-22/most-asked-question/app.js
@@ -0,0 +1,48 @@
+
+
+// var a = 20;
+
+// function meraFunction(){
+//     console.log("inside meraFunction")
+// }
+
+// console.log(a);
+// meraFunction();
+
+// ----------------------------
+// ----- HOISTING -------
+// whenever we access the variable or the funciton before its declaration then it is called hoisting
+
+// meraFunction(); //use
+// console.log(a); //use
+
+// var a = 20; //define
+
+// function meraFunction(){ //define
+//     console.log("inside meraFunction")
+// }
+
+// ----------------------------
+// hoiting
+function a(){ //define
+    console.log(b); //use
+    var b = 100 //define
+}
+
+a(); //access
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
