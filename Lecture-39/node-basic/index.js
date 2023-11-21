5cada2d03d0e4b8335f88c53f18d1f33910cee3f
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-11-21 21:59:20 +0530

    lecture 39 codes

---
+++ Lecture-39/node-basic/index.js
@@ -0,0 +1,14 @@
+let pi = 3.145
+
+let favNumber = 7
+
+function sqr(num){
+    return num*num
+}
+
+let ans = sqr(6);
+
+
+console.log(pi)
+console.log(favNumber)
+console.log(ans)
---
+++ Lecture-39/process/app.js
@@ -0,0 +1,27 @@
+
+
+// console.log(process.argv);
+
+
+
+
+// let arr = process.argv.slice(2);
+// // console.log(arr);
+// for(let item of arr){
+//     console.log(item)
+// }
+
+
+// -----------------------
+
+
+let n = process.argv.pop();
+// console.log(n);
+
+for(let item = 1 ; item <= n ; item ++){
+    console.log(item)
+}
+
+
+
+
